0a714bcf8431fc350150117e62df6a50e4ce2ea1
m3gh4l4 <meghala.pasikanti@gmail.com>
2021-05-09 14:49:33 +0530

    modified main page

--- about.js
+++ about.js
@@ -1,15 +1,19 @@
-$(document).ready(function () {
-    $('#skills').click(function () {
-        $('.two').addClass("addClass")
-        $('#up').attr("src","images/4.png")
-        $('#skills').click(function() {
-            // $('.two').removeClass('addClass')
-            // $('#up').attr("src","images/1.png")
-            setTimeout(() => {
-                $('.two').removeClass('addClass')
-            })
-        })
-        // alert("here is ur skyblue")
-    })
+// $(document).ready(function () {
+//     $('#skills').click(function () {
+//         $('.two').addClass("addClass")
+//         $('#up').attr("src","images/4.png")
+//         $('#skills').click(function() {
+//             // $('.two').removeClass('addClass')
+//             // $('#up').attr("src","images/1.png")
+//             setTimeout(() => {
+//                 $('.two').removeClass('addClass')
+//             })
+//         })
+//         // alert("here is ur skyblue")
+//     })
 
-})
+// })
+
+let w = window.innerWidth;
+let h = window.innerHeight;
+
