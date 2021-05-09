186a4bccde62e5d111fc434a4240e03a66ac9d0c
m3gh4l4 <meghala.pasikanti@gmail.com>
2021-05-09 16:53:19 +0530

    added animation to slider

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
-var speed=150;
-var i=0;
-var text="I am a computer science undergrad at NITW making this website . ";
+let speed=150;
+let i=0;
+let text="I am a computer science undergrad at NITW making this website . ";
 
 function typeWriter() 
 {
@@ -13,22 +13,84 @@ function typeWriter()
     }
 }
 
-$('.multi-item-carousel').carousel({
-    interval: false
-});
-
-$('.multi-item-carousel .item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
+function myFunction(num)
+{
+  let precls = "";
+  let currcls = "";
+  let nextcls = "";
+  if (num == 1) {
+    precls = "zero";
+    currcls = "one";
+    nextcls = "two";
+  }
+  else if(num == 2) {
+    precls = "one";
+    currcls = "two";
+    nextcls = "three";
+  }
+  else if(num == 3) {
+    precls = "two";
+    currcls = "three";
+    nextcls = "four";
+  }
+  else if(num == 4){
+    precls = "three";
+    currcls = "four";
+    nextcls = "five";
+  }
+  else {
+    precls = "four";
+    currcls  = "five";
+    nextcls = "six";
+  }
+  console.log(precls + currcls + nextcls);
+  let x = document.getElementsByClassName(currcls);
+  //expanding the slide
+  if (x[0].classList.contains('addClass'))
+  {
+    // document.getElementById('up' + currcls).style.transform = "scaleY(1)"
+    //inverting the arrow button
+    if (document.getElementById('up' + currcls).classList.contains('downAni'))
+    {
+      console.log("contains downAni ->" + currcls )
+      document.getElementById('up' + currcls).classList.remove('downAni')
     }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    if (next.next().length>0) {
-      next.next().children(':first-child').clone().appendTo($(this));
-    } else {
-        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    document.getElementById('up' + currcls).classList.add('upAni')
+    x[0].classList.add('removeClass');
+    console.log("removed class")
+    x[0].classList.remove('addClass');
+  }
+  else {
+    // document.getElementById('up').style.transform = "scaleY(-1)"
+    //inverting the arrow button
+    if (document.getElementById('up' + currcls).classList.contains('upAni'))
+    {
+      console.log("contains upAni ->" + currcls )
+      document.getElementById('up' + currcls).classList.remove('upAni')
     }
-  });
-
-
+    document.getElementById('up' + currcls).classList.add('downAni')
+    x[0].classList.add('addClass');
+    console.log("added class")
+    x[0].classList.remove('removeClass')
+  }
+  let wrapper = document.getElementsByClassName('swiper-wrapper')[0];
+  // setTimeout(() => {
+  //   for(let i=0;i<5;i++)
+  //   {
+  //     let others = wrapper.children[i].classList[1];
+  //     if(others != currcls)
+  //     {
+  //       if(document.getElementById(others).style.display != "none")
+  //       {
+  //         console.log("not none")
+  //         document.getElementById(others).style.display = "none";
+  //       }
+  //       else 
+  //       {
+  //         console.log("none")
+  //         document.getElementById(others).style.display = "inline-block";
+  //       }
+  //     }
+  //   }
+  // }, 1000);  
+}
