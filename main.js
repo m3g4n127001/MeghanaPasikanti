036bff8f4cfd7a72f17685de7fc69eb8bff46120
m3g4n127001 <meghana.pasikanti@gmail.com>
2021-05-13 01:10:42 +0530

    slider transitions

--- main.js
+++ main.js
@@ -12,6 +12,42 @@ function typeWriter()
         i++;
         setTimeout(typeWriter,speed);
     }
-    console.log(swiper.activeIndex);
+}
+
+let expanded = false;
+function myFunction(num)
+{
+    console.log(num);
+    let ele = document.getElementsByClassName("swiper-slide-active");
+    let elep=document.getElementsByClassName("swiper-slide-prev");
+    if(expanded)
+    {
+        console.log(ele[0].classList);
+        console.log(elep[0].classList);
+        expanded = false;
+        console.log('if')
+        elep[0].classList.remove('outani')
+        elep[0].classList.add('inani')
+        ele[0].classList.remove('outani')
+        ele[0].classList.remove('upani')
+        ele[0].classList.add('downani')
+        
+    }
+    else {
+        console.log(ele[0].classList);
+        console.log(elep[0].classList);
+        expanded = true;
+        console.log('else')
+        elep[0].classList.remove('inani')
+        ele[0].classList.remove('inani')
+        elep[0].classList.add('outani')
+        ele[0].classList.remove('downani')
+        ele[0].classList.add("upani");
+        console.log(swiper.params)
+        swiper.params.grabCursor=false;
+        swiper.unsetGrabCursor();
+        
+    }
+
 }
 
