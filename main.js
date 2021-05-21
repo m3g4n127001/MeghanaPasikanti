00631851095087e8ccbd2130bcb4d1fc03948d7d
m3g4n127001 <meghana.pasikanti@gmail.com>
2021-05-21 22:25:34 +0530

    resp2

--- main.js
+++ main.js
@@ -20,27 +20,9 @@ function myFunction(num)
     document.getElementsByClassName('dropdown-list-res')[0].style.display="none";
     let ele = document.getElementsByClassName("swiper-slide-active");
     let elep=document.getElementsByClassName("swiper-slide-prev");
-    // window.onresize = function() {
-    //     expanded = true;
-    //     document.getElementsByClassName('swiper-slide-active')[0].classList.add('center')
-    //     myFunction(0)
-    // }
-    // if(document.getElementById('four').classList.contains('swiper-slide-active') || document.getElementById('four').classList.contains('swiper-slide-duplicate-active')) {
-    //     console.log(document.getElementById('four').classList)
-    //     console.log('hope')
-    //     console.log(document.getElementsByClassName('container')[3].childNodes)
-    //     setTimeout(() => {
-    //         document.getElementById('tag').classList.add('arttagani')
-    //     }, 1000);
-    // }
-    // else {
-    //     console.log(document.getElementById('four').classList)
-    //     console.log('no hope')
-    // }
     if(expanded)
     {
         expanded = false;
-
         r.style.setProperty('--height', '40vh')
         r.style.setProperty('--scroll', 'hidden')
         elep[0].classList.remove('outani')
